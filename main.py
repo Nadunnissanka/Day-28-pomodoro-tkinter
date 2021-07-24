e2a16cde0e88d3e049eddf4fb9472650844bb025
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-07-24 17:50:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro V1")
+window.minsize(width=500, height=500)
+window.config(padx=20, pady=20)
+
+
+window.mainloop()
